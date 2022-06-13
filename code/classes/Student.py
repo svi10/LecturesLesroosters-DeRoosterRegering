2182a0c7208a0f857e08edc2e35336f9a7cb565b
Sharon <sharonvisser1@gmail.com>
2022-06-13 13:23:11 +0200

    Class Student

---
+++ code/classes/Student.py
@@ -0,0 +1,8 @@
+class Student:
+    "This class includes all student information"
+
+    def __init__(self):
+        self._studentnumber, self._name = helpers.import_information()
+        self._activities
+        self._courses_df
+        self._malus_points

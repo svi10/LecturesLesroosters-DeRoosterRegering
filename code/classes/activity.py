23f72a8ab820b548bd459df43b716a2a570599d4
Sharon <sharonvisser1@gmail.com>
2022-06-14 13:32:16 +0200

    Add class Activity

--- code/classes/activity.py
+++ code/classes/activity.py
@@ -1,18 +1,41 @@
 
 class Activity:
+    """
+    This class contains the activitytype, the name of the activity and the list of students. 
+    It can add, remove and show students.
+    """
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, activitytype, name, student_list):
+        self._type = activitytype
+        self._name = name
+        self._student_set = set(student_list)
+        self._room = None
+        self._timeslot = None
 
-    def show_schedule(self):
-        pass
+    def add_student(self, students):
+        self._student_set.update(students)
 
-    def add_student(self):
-        pass
-
-    def remove_student(self):
-        pass
+    def remove_student(student):
+        self._student_set.discard(student)
 
     def show_students(self):
-        pass
+        return self._student_set
 
+    def get_name(self):
+        return self._name
+
+# if __name__ == "__main__":
+#     activitytype = "tutorial"
+#     course = "Algoritmen"
+#     student_list = ["Student1", "Student2", "Student3"]
+#     activity = Activity(activitytype, course, student_list)
+#     students = activity.show_students()
+#     print(students)
+
+#     new_students = ["Student4"]
+#     activity.add_student(new_students)
+#     students = activity.show_students()
+#     print(students)
+
+#     print(activity)
+#     print(activity.get_name())
